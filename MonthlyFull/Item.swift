0fa8f74976a3ca5f30b52e8fa487624c3a710ecf
“Michael <240designworks@gmail.com>
2025-02-13 09:22:34 -0600

    Initial Commit

---
+++ MonthlyFull/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MonthlyFull
+//
+//  Created by Michael Martell on 2/13/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
